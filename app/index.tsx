7dac2dbf89928197a57d34e5a4c0cc8a3178e332
bramr03 <bramrutgers14@gmail.com>
2026-06-06 12:33:32 +0200

    fucked up and fixed index

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,5 @@
-import { Text, View } from 'react-native'
+import { Redirect } from 'expo-router'
 
 export default function Index() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>DateDiner werkt!</Text>
-    </View>
-  )
+  return <Redirect href="/(auth)/login" />
 }
